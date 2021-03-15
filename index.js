01f1d84950979cf0fe37dda79c29e342d396a701
ehsanahmedsadid <ehsanahmed305@gmail.com>
2021-03-15 14:41:14 +0600

    v 1.1

--- index.js
+++ index.js
@@ -3,26 +3,31 @@ const {readFile} = require('fs')
 
 const app = express();
 
-app.get('/', (reqest, response) => {
-    
-    readFile('./index.html', 'utf-8', (err,html) => {
+app.get('/', (request, response) => {
+    console.log(request.url);
+    readFile('./index.html', 'utf-8', (err,file) => {
         if(err){
             response.status(500).send("sorry, out of order");
         }
-        response.send(html);
+        response.send(file);
     });
     
 
 });
-app.get('/favicon.ico', (reqest, response) => {
-    
-    readFile('./favicon.ico', (err, html) => {
+app.get('/favicon.ico', (request, response) => {
+    console.log(request.url);
+    readFile('./favicon.ico', (err, icon) => {
         if(err){
             response.status(500).send("sorry, out of order");
         }
-        response.send(html);
+        response.send(icon);
     });
     
 
 });
+app.get('/%F0%9F%98%AD', (request, response) => {
+    console.log(request.url)
+    response.send("<h1>why you crying?</h1>");
+    });
+    
 app.listen(process.env.PORT || 3000, ()=> console.log("App stared at http://localhost:3000"));
